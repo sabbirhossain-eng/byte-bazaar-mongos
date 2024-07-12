4b458579d5135cbe65a946d3360f383cd7c0ab07
Sabbir Hossain <sabbirh8181@gmail.com>
2024-07-12 19:55:54 +0600

    bottom navbar add

--- src/Components/Shear/Navbar/Navbar.jsx
+++ src/Components/Shear/Navbar/Navbar.jsx
@@ -1,62 +1,108 @@
 import Container from "../Container/Container";
 import logo from "../../../assets/Byte Bzaar logo.png";
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation} from 'react-router-dom';
 import { MdCardGiftcard } from "react-icons/md";
 import { GiElectric } from "react-icons/gi";
 import { IoPerson } from "react-icons/io5";
 import { BsCartCheckFill } from "react-icons/bs";
+import "./Navbar.css";
+import Typography from '@mui/material/Typography';
+import { HashLink } from "react-router-hash-link";
 
 const Navbar = () => {
   const { pathname } = useLocation();
+  const handleClick = () => {
+    
+    window.location.reset(true);
+  };
+
   const menuList = (
     <>
-      <li className="nav-link relative">
+      <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:justify-between lg:gap-6">
+      <Typography className="nav-link relative">
         <Link
+         onClick={handleClick}
           className={` ${
-            pathname === "/" ? "text-[#076aa5] font-bold" : "text-gray-700"
-          } no-underline font-semibold text-lg hover:bg-[#076aa5] lg:hover:bg-none text-gray-800 hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
+            pathname === "/" ? "text-primary" : "text-gray-700 dark:text-gray-200"
+          } no-underline font-semibold   lg:hover:bg-none text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
           to={"/"}
         >
-          Home
+          Home 
         </Link>
-      </li>
-      <li className="nav-link relative">
+      </Typography>
+      <Typography className="nav-link relative">
         <Link
           className={` ${
-            pathname === "/" ? "text-[#076aa5] font-bold" : "text-gray-700"
-          } no-underline font-semibold text-lg hover:bg-[#076aa5] lg:hover:bg-none text-gray-800 hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
+            pathname === "/laptop"
+              ? "text-primary"
+              : "text-gray-700 dark:text-gray-200"
+          } no-underline font-semibold  text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
           to={"/laptop"}
         >
           Laptop
         </Link>
-      </li>
-      <li className="nav-link relative">
+      </Typography>
+      <Typography className="nav-link relative">
         <Link
           className={` ${
-            pathname === "/" ? "text-[#076aa5] font-bold" : "text-gray-700"
-          } no-underline font-semibold text-lg hover:bg-[#076aa5] lg:hover:bg-none text-gray-800 hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
-          to={"/about"}
+            pathname === "/desktop"
+              ? "text-primary"
+              : "text-gray-700 dark:text-gray-200"
+          } no-underline font-semibold  text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
+          to={"/desktop"}
         >
-          About
+          Desktop
         </Link>
-      </li>
-      <li className="nav-link relative">
+      </Typography>
+      <Typography className="nav-link relative">
         <Link
           className={` ${
-            pathname === "/" ? "text-[#076aa5] font-bold" : "text-gray-700"
-          } no-underline font-semibold text-lg hover:bg-[#076aa5] lg:hover:bg-none text-gray-800 hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
-          to={"/contact"}
+            pathname === "/monitor"
+              ? "text-primary"
+              : "text-gray-700 dark:text-gray-200"
+          } no-underline font-semibold  text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
+          to={"/monitor"}
         >
+          Monitor
+        </Link>
+      </Typography>
+      <Typography className="nav-link relative">
+        <Link
+          className={` ${
+            pathname === "/blog"
+              ? "text-primary"
+              : "text-gray-700 dark:text-gray-200"
+          } no-underline font-semibold   lg:hover:bg-none text-gray-800 dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
+          to={"/blog"}
+        >
+          Blog
+        </Link>
+      </Typography>
+      <Typography className="nav-link relative">
+        <Link
+          className={` ${
+            pathname === "/contactUs" ? "text-primary" : "text-gray-700 dark:text-gray-200"
+          } no-underline font-semibold   lg:hover:bg-none text-gray-800  dark:hover:text-white p-4 lg:p-0 inline-block rounded-[10px] w-full`}
+          
+        >
+          <HashLink smooth to="/#contactUs">
           Contact Us
+          </HashLink>
         </Link>
-      </li>
+      </Typography>
+    </ul>
     </>
   );
   return (
-    <div className=" border-b border-gray-300 bg-black">
+    <div>
+      {/* nav top */}
+      <div className=" border-b border-gray-300 bg-black">
       <Container>
         <div className="flex p-2 justify-between items-center flex-wrap">
-          <div className="flex items-center">
+          {/* logo */}
+          <div 
+          onClick={handleClick}
+          className="flex items-center">
             <img src={logo} className="" />
           </div>
           {/* Search */}
@@ -77,24 +123,24 @@ const Navbar = () => {
             </svg>
           </div>
           {/* nav stat */}
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-6">
             {/* Offer */}
           <div className="w-full">
             <div className="flex items-center shadow-sm rounded-md">
-                <div className="text-4xl text-secondary">
+                <div className="text-2xl text-secondary">
                     <MdCardGiftcard/>
                 </div>
 
                 <div className="mx-3">
                     <h4 className="text-xl font-semibold text-white">Offers</h4>
-                    <div className="text-gray-400">Latest Offers</div>
+                    <div className="text-gray-400 text-nowrap">Latest Offers</div>
                 </div>
             </div>
         </div>
             {/* Happy hour */}
           <div className="w-full">
             <div className="flex items-center shadow-sm rounded-md">
-                <div className="text-4xl text-secondary">
+                <div className="text-2xl text-secondary">
                     <GiElectric/>
                 </div>
                 <div className="mx-3 text-nowrap">
@@ -106,7 +152,7 @@ const Navbar = () => {
             {/* Account */}
           <div className="w-full">
             <div className="flex items-center shadow-sm rounded-md">
-                <div className="text-4xl text-secondary">
+                <div className="text-2xl text-secondary">
                     <IoPerson/>
                 </div>
                 <div className="mx-3 text-nowrap">
@@ -126,18 +172,28 @@ const Navbar = () => {
         {/* Cart */}
         <div className="w-full">
             <div className="flex items-center shadow-sm rounded-md">
-                <div className="text-4xl text-secondary">
+                <div className="text-2xl text-secondary relative">
                     <BsCartCheckFill/>
+                    <p className="absolute bottom-3 left-4 bg-primary text-white rounded-full px-1 text-sm">0</p>
                 </div>
                 <div className="mx-3 text-nowrap">
                     <h4 className="text-xl font-semibold text-white">Cart</h4>
-                    <p className="text-white bg-secondary rounded-full w-6 px-2">0</p>
                 </div>
             </div>
         </div>
           </div>
         </div>
       </Container>
+    </div>
+
+      {/* nav bottom */}
+      <div className="bg-slate-50">
+        <Container>
+        <div>
+          {menuList}
+        </div>
+        </Container>
+      </div>
     </div>
   );
 };
