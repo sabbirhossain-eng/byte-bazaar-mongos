58f3b24394bc20be936989ef6e627a7057dbf499
Sabbir Hossain <sabbirh8181@gmail.com>
2024-09-03 01:41:00 +0600

    comlipte the sign up page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
+// import { useLocation, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { TextField } from "@mui/material";
 import OutlinedInput from "@mui/material/OutlinedInput";
@@ -10,10 +9,15 @@ import InputAdornment from "@mui/material/InputAdornment";
 import IconButton from "@mui/material/IconButton";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import LockIcon from '@mui/icons-material/Lock';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import GoogleAndFacebookLogin from "../../Components/Button/GoogleAndFacebookLogin/GoogleAndFacebookLogin";
+import LoadingButton from '@mui/lab/LoadingButton';
+import SendIcon from '@mui/icons-material/Send';
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
+//   const [loading, setLoading] = useState(true);
+  
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -37,12 +41,22 @@ const SignIn = () => {
     const password = form.password.value;
     console.log(email, password);
   };
+
+//   button loading
+// function handleClick() {
+//     setLoading(true);
+//   }
+  
   return (
     <div className="mt-20">
         <div className="text-center mb-4">
-            <LockIcon color="primary"/>
-            <h3 className="text-2xl font-semibold">Sign in</h3>
+            <span className="bg-primary p-3 pb-4 rounded-full items-center text-white"><LockOutlinedIcon/></span>
+            <h3 className="text-2xl font-semibold mt-4">Sign in</h3>
             <p>Welcome user, please sign in to continue</p>
+        </div>
+        {/* google facebook button */}
+        <div>
+            <GoogleAndFacebookLogin/>
         </div>
       <div className="card flex-shrink-0 w-full max-w-sm mx-auto max-h-[100-60vh]">
         <form onSubmit={handleLogin} className="">
@@ -94,17 +108,21 @@ const SignIn = () => {
           </label>
 
           <div className="form-control mt-6">
-            <input
-              className="btn bg-primary-light"
-              type="submit"
-              value="Sign In"
-            />
+          <LoadingButton
+          size="small"
+          endIcon={<SendIcon />}
+        //   loading={loading}
+          loadingPosition="end"
+          variant="contained"
+        >
+          Send
+        </LoadingButton>
           </div>
-          <p className="text-center">
+          <p className="text-center text-lg">
             {/* <SocialLogin/> */}
             <small>
               New here?{" "}
-              <Link to="/signUp" className="text-primary-light">
+              <Link to="/signUp" className="text-primary">
                 Create a New Account
               </Link>
             </small>
