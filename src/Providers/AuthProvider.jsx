9b72161fb84dd4f67a55ab35238cc6a3ec53c70e
Sabbir Hossain <sabbirh8181@gmail.com>
2024-09-11 00:42:59 +0600

    axios install and baseurl setup

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,5 +1,18 @@
+import { createContext } from "react";
+import {
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateProfile,
+  } from "firebase/auth";
+import app from "../Firebase/Firebase.config";
 
-
+const auth = getAuth(app);
+export const AuthContext = createContext(null);
 const AuthProvider = () => {
     return (
         <div>
