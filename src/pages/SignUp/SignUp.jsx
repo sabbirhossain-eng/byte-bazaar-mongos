bccc66b78c28064a3bdf5278fbf2ca6cdd72f6dd
Sabbir Hossain <sabbirh8181@gmail.com>
2024-09-12 01:59:33 +0600

    use auth hook create and signUp page from event hendler add

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 // import { useLocation, useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { TextField } from "@mui/material";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
@@ -17,8 +17,12 @@ import { MuiTelInput } from 'mui-tel-input';
 
 const SignUp = () => {
     const [showPassword, setShowPassword] = useState(false);
-//   const [loading, setLoading] = useState(true);
-    const [phone, setPhone] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [phone, setPhone] = useState('');
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -30,28 +34,36 @@ const SignUp = () => {
     event.preventDefault();
   };
 
-  // const navigate = useNavigate();
-  // const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-  // const from = location.state?.from?.pathname || "/";
-
-  const handleLogin = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const email = form.email.value;
-    const password = form.password.value;
-    console.log(email, password);
-  };
+  const from = location.state?.from?.pathname || "/";
 
 //   phone number handel
-    const handlePhoneChange = (newPhone) => {
-    setPhone(newPhone)
+  //   const handlePhoneChange = (newPhone) => {
+  //   setPhone(newPhone)
+  // }
+
+  // button loading
+
+function handleClick() {
+    setLoading(true);
   }
 
-//   button loading
-// function handleClick() {
-//     setLoading(true);
-//   }
+const handleLogin = (event) => {
+  event.preventDefault();
+  
+  const first_Name = firstName || '';
+  const last_Name = lastName || '';
+  const newEmail = email || '';
+  const newPassword = password || '';
+  const phoneNumber = phone || '';
+  console.log(first_Name, last_Name, newEmail, newPassword, phoneNumber);
+  
+  // navigate(from);
+  // setLoading(false);
+  
+};
   
   return (
     <div className="mt-16 max-w-sm mx-auto">
@@ -76,6 +88,7 @@ const SignUp = () => {
           <div className="flex flex-col gap-4">
             <div className="flex gap-4">
               <TextField
+              onChange={(e)=> setFirstName(e.target.value)}
                 id="outlined-first-name-input"
                 label="First Name"
                 type="text"
@@ -85,6 +98,7 @@ const SignUp = () => {
                 fullWidth
               />
               <TextField
+              onChange={(e)=> setLastName(e.target.value)}
                 id="outlined-last-name-input"
                 label="Last Name"
                 type="text"
@@ -96,6 +110,7 @@ const SignUp = () => {
             </div>
             <div>
               <TextField
+              onChange={(e)=> setEmail(e.target.value)}
                 className=""
                 id="outlined-email-input"
                 label="Email"
@@ -113,6 +128,7 @@ const SignUp = () => {
                 Password
               </InputLabel>
               <OutlinedInput
+              onChange={(e)=> setPassword(e.target.value)}
                 id="outlined-adornment-password"
                 type={showPassword ? "text" : "password"}
                 size="small"
@@ -146,16 +162,18 @@ const SignUp = () => {
           required
           size="small"
           fullWidth
-          onChange={handlePhoneChange} />
+          onChange={(newPhone) => setPhone(newPhone)} />
           </div>
 
           <div className="form-control mt-6">
           <LoadingButton
           size="small"
           endIcon={<SendIcon />}
-        //   loading={loading}
+          loading={loading}
           loadingPosition="end"
           variant="contained"
+          onClick={handleClick}
+          type="submit"
         >
           Send
         </LoadingButton>
