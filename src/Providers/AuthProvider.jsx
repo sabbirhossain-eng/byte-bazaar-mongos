3c06b7ad1ec636e4eaa50472d53e3334e6652b37
Sabbir Hossain <sabbirh8181@gmail.com>
2024-09-11 20:59:03 +0600

    atuth provider createb user

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -10,11 +10,25 @@ import {
     updateProfile,
   } from "firebase/auth";
 import app from "../Firebase/Firebase.config";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
 
 const auth = getAuth(app);
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({children}) => {
+    const googleProvider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [userRole, setUserRole] = useState(null);
+  const [userId, setUserId] = useState(null);
+
+  const axiosPublic = useAxiosPublic();
+
+  const createUser = (email, password) =>{
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
 
     const authInfo = {
 
@@ -22,7 +36,6 @@ const AuthProvider = ({children}) => {
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
-            
         </AuthContext.Provider>
     );
 };
