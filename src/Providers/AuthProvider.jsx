f610e103503f9b4b9ce7ca8c10bb0439e64aed79
Sabbir Hossain <sabbirh8181@gmail.com>
2024-09-11 22:44:28 +0600

    signIn auth add

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -29,9 +29,14 @@ const AuthProvider = ({children}) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword (auth, email, password);
+  };
 
     const authInfo = {
-
+            createUser,
+            signIn
     }
     return (
         <AuthContext.Provider value={authInfo}>
